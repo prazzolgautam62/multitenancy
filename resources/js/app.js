0099080e1259a2b8caa09eb69d1d3cfcd43ab7de
Prajwal Gautam <prajwal.ingrails@gmail.com>
2024-04-05 09:54:56 +0545

    changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
+import App from './App.vue';
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+   
+  ],
+});
+const app = createApp(App);
+app.use(router);
+app.use(ToastPlugin);
+app.mount('#app');
